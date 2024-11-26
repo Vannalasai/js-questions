bc816dce0546c78af8b9422f0ba51a9fc75149cd
vannala sai <saikrishna.vannala@gmail.com>
2024-11-26 23:10:39 +0530

    1

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('myButton')
+const name = document.getElementById('myName')
+
+button.addEventListener('click', function() {
+  name.innerHTML = "Sai Vannala"
+})
